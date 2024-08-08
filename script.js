d2c56a507fe7e416d15206cfcfec119c5337dd25
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-08-08 17:09:14 +0200

    cuadrado

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ const container = document.getElementById("container")
 const color = ["df2222", "224bdf", "df22ac","22df3e", "d2df22"]
 const squares = 500
 
-for(let i = 0, i < squares, i++) {
-    const cuadrado= document.createElement("div")
-    cuadrado.classList.add("cuadrado")
+
+for(let i = 0; i < squares; i++) {
+const cuadrado = document.createElement ("div")
+cuadrado.classList.add("cuadrado")
+
+container.appendChild(cuadrado)
 }
 
-container.forEach(cuadrado => {
-    cuadrado.addEventListener ("mouseover", () => {
-        cuadrado.style.backgroundColor = "df2222"
-    })
-    
-});
+
+function recorrerCuadrado() {
+    const cuadrados = container.querySelectorAll(".cuadrado")
+    container.forEach(cuadrado => {
+        cuadrado.addEventListener ("mouseover", () => {
+            cuadrado.style.backgroundColor = colors()
+            })    
+    }) 
+}
+
+
+
+function colors(){
+    return Math.floor(Math.random() * color.length)
+}
+
+recorrerCuadrado()

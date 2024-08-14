633206a87de795d325067865e43ac0f604fde578
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-08-14 18:13:31 +0200

    terminado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById("container");
-const color = ["df2222", "224bdf", "df22ac", "22df3e", "d2df22", "13aa83"];
+const color = ["df2222", "224bdf", "df22ac", "22df3e", "d2df22", "13aa83", "76124a"];
 const squares = 500;
 
 for (let i = 0; i < squares; i++) {
@@ -13,12 +13,12 @@ function recorrerCuadrado() {
   cuadrados.forEach(cuadrado => {
     // Evento para cambiar el color al pasar el mouse
     cuadrado.addEventListener("mouseover", () => {
-      cuadrado.style.backgroundColor = `#${color[numeroAleatorio()]}`;
+      cuadrado.style.backgroundColor = `#${color[numeroAleatorio()]}`;//al poner # se indica que es un color lo que hay dentro del template
     });
     
     // Evento para volver al color inicial al quitar el mouse
     cuadrado.addEventListener("mouseout", () => {
-      cuadrado.style.backgroundColor = ""; // Aquí debes poner el color inicial que desees, si es blanco, por ejemplo: "#ffffff"
+      cuadrado.style.backgroundColor = "#4a4949"; // Aquí debes poner el color inicial que desees, si es blanco, por ejemplo: "#ffffff"
     });
   });
 }
